bfb6c6f8cb6bae05fb1616f8c44660d355f1ce69
Marta Krawczyk <martaalicja113@gmail.com>
2020-05-12 18:45:30 -0400

    Comitting Changes

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,27 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
 // import data 
+ import data from "../../dummy-data.js";
+//console.log(data)
+
 
 const PostsPage = () => {
   // set up state for your data
+  const [usersData , setUsersData] = useState(data)
+//console.log(usersData[0].comments[1])
+
   return (
     <div className="posts-container-wrapper">
       {/* map through data here to return a Post and pass data as props to Post */}
+
+      {usersData.map((userObj) => {
+        console.log(userObj)
+        return <Post key={userObj.username} post={userObj}/>
+      })}
+    
     </div>
   );
 };
